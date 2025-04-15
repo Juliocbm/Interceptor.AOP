4a2a5604c972e81195a926fa149bb5a65f45e4bf
Julio Cesar Bautista Monsalvo <desarrollohg@hgtransportaciones.com>
2025-04-15 16:58:00 -0600

    Se añade icono

--- Interceptor.AOP/Utilities/ProxyFactory.cs
+++ Interceptor.AOP/Utilities/ProxyFactory.cs
@@ -7,16 +7,6 @@ using System.Threading.Tasks;
 
 namespace Interceptor.AOP.Utilities
 {
-    //public static class ProxyFactory
-    //{
-    //    public static T Create<T>(T decorated, ILogger logger, Func<Exception, Task>? callback = null)
-    //    {
-    //        var proxy = DispatchProxy.Create<T, ExceptionInterceptor<T>>();
-    //        ((ExceptionInterceptor<T>)(object)proxy).Configure(decorated, logger, callback);
-    //        return proxy;
-    //    }
-    //}
-
     public static class ProxyFactory
     {
         public static TInterface Create<TInterface>(TInterface decorated, ILogger logger, InterceptorOptions options = null)
